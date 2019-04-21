956c418ba7946331687790c3181a8174e05d2e1b
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2019-04-21 16:38:29 -0500

    Add the first component to test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,5 +3,11 @@ import React from 'react';
 import App from '../src/App';
 
 describe('App', () => {
-
+    describe('#Text reservation code', () => {
+        it('should render the code of reservation in testId reserveCode', () => {
+            const app = shallow(<App />);
+            const text = app.find("[testID='reserveCode']").dive().text();
+            expect(text).toEqual('ABC-DEF-HJ');
+        });
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
-import Thank from './Containers/Checkout/ThankYou/content'
+import { StyleSheet, View, Text } from 'react-native'
 
 export default class App extends Component<any, any> {
   render() {
     return (
       <View style={styles.container}>
-        <Thank />
+        <Text style={styles.idReserve} testID={'reserveCode'} >{'ABC-DEF-GHJ'}</Text>
       </View>
     )
   }
@@ -14,7 +13,14 @@ export default class App extends Component<any, any> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    margin: 15
+    flex: 1
+  },
+  idReserve: {
+    fontFamily: 'Cochin',
+    fontSize: 22,
+    lineHeight: 24,
+    textAlign: 'center',
+    color: 'rgb(23,158,164)',
+    paddingTop: 6
   }
 })
