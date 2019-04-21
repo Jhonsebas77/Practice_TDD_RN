f0fffe985d18af1459efbc5c9ad4f5c695fb6e6d
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2019-04-20 23:40:13 -0500

    Add the final test to basic component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,40 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native'
 
 export default class App extends Component<any, any> {
+  constructor(props) {
+    super(props)
+    this.state = {
+      length: 0,
+      width: 0
+    }
+    this.onUpdateLength = this.onUpdateLength.bind(this)
+    this.onUpdateWidth = this.onUpdateWidth.bind(this)
+    this.onUpdateArea = this.onUpdateArea.bind(this)
+  }
+
+  onUpdateLength(length) {
+    this.setState({ length })
+  }
+
+  onUpdateWidth(width) {
+    this.setState({ width })
+  }
+
+  onUpdateArea() {
+    const area = this.state.length * this.state.width
+    this.setState({ area })
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text testID={'rectangleLengthText'} style={styles.label}>Length</Text>
-        <TextInput testID={'rectangleLengthInput'} />
+        <TextInput testID={'rectangleLengthInput'} onChangeText={this.onUpdateLength} />
         <Text testID={'rectangleWidthText'} style={styles.label}>Width</Text>
-        <TextInput testID={'rectangleWidthInput'} />
-
-        <TouchableHighlight testID={'rectangleAreaButton'} style={styles.button}>
+        <TextInput testID={'rectangleWidthInput'} onChangeText={this.onUpdateWidth} />
+        <TouchableHighlight testID={'rectangleAreaButton'} style={styles.button} onPress={this.onUpdateArea}>
           <Text style={styles.textButton}>Calculate</Text>
         </TouchableHighlight>
+        <Text testID={'rectangleAreaResult'}>{this.state.area}</Text>
       </View>
     )
   }
