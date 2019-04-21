56df58de9aee6971e8bc788b1e9612983d3eab41
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2019-04-21 00:34:22 -0500

    Begin with the test Seriuosly

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import Thank from './Containers/Checkout/ThankYou/content'
 
 export default class App extends Component<any, any> {
-  constructor(props) {
-    super(props)
-    this.state = {
-      length: 0,
-      width: 0
-    }
-    this.onUpdateLength = this.onUpdateLength.bind(this)
-    this.onUpdateWidth = this.onUpdateWidth.bind(this)
-    this.onUpdateArea = this.onUpdateArea.bind(this)
-  }
-
-  onUpdateLength(length) {
-    this.setState({ length })
-  }
-
-  onUpdateWidth(width) {
-    this.setState({ width })
-  }
-
-  onUpdateArea() {
-    const area = this.state.length * this.state.width
-    this.setState({ area })
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text testID={'rectangleLengthText'} style={styles.label}>Length</Text>
-        <TextInput testID={'rectangleLengthInput'} onChangeText={this.onUpdateLength} />
-        <Text testID={'rectangleWidthText'} style={styles.label}>Width</Text>
-        <TextInput testID={'rectangleWidthInput'} onChangeText={this.onUpdateWidth} />
-        <TouchableHighlight testID={'rectangleAreaButton'} style={styles.button} onPress={this.onUpdateArea}>
-          <Text style={styles.textButton}>Calculate</Text>
-        </TouchableHighlight>
-        <Text testID={'rectangleAreaResult'}>{this.state.area}</Text>
+        <Thank />
       </View>
     )
   }
@@ -45,19 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 15
-  },
-  button: {
-    backgroundColor: '#414f6f',
-    borderRadius: 10,
-    marginTop: 20,
-    padding: 15
-  },
-  textButton: {
-    fontSize: 20,
-    alignSelf: 'center',
-    color: '#ffffff'
-  },
-  label: {
-    fontSize: 17
   }
 })
