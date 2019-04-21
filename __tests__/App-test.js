31aca1090235d703a2b779c46a756da018d4b827
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2019-04-21 17:21:00 -0500

    Refactor the test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,14 +3,12 @@ import React from 'react';
 import App from '../src/App';
 
 describe('App', () => {
-    describe('#Text subtitle of reservation code', () => {
+    describe('#Container reservation code', () => {
         it('should render the subtitle fot reservation code', () => {
             const app = shallow(<App />);
             const text = app.find("[testID='subtitleReservationCode']").dive().text();
             expect(text).toEqual('Tu codigo de reserva Bayou es');
         });
-    });
-    describe('#Text reservation code', () => {
         it('should render the reservation code for default', () => {
             const app = shallow(<App />);
             app.setState({
